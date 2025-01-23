5f3247a3f12efb351145fd5416dbcd960cd2755d
Aradhya Nepal <aradhya.1221@gmail.com>
2025-01-23 19:23:39 +0545

    Initial Commit

---
+++ src/main/java/com/a2/mobile_compatible_payment_api/MobileCompatiblePaymentApiApplication.java
@@ -0,0 +1,13 @@
+package com.a2.mobile_compatible_payment_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileCompatiblePaymentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileCompatiblePaymentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/a2/mobile_compatible_payment_api/MobileCompatiblePaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a2.mobile_compatible_payment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileCompatiblePaymentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
